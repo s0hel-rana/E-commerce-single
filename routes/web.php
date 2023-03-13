544374dbac5b68371f410e39c62040769b255b55
Sohel Rana <sohelrananull@gmail.com>
2023-03-13 22:10:25 +0600

    user template

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',function(){
+    return view('user.layouts.template');
 });
 
 Route::get('/dashboard', function () {
