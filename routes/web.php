c82c7ed8be37340ca64f8150127274a5b474ab2c
Sohel Rana <sohelrananull@gmail.com>
2023-03-15 23:45:49 +0600

    user template

--- routes/web.php
+++ routes/web.php
@@ -5,25 +5,14 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\SubCategoryController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/',function(){
-    return view('user.layouts.template');
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/', 'index');
+
 });
 
 Route::get('/dashboard', function () {
